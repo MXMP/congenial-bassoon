9b88c6efcd5a26fef2e93a399a9b2fcd2d6fa31a
mxmp <bandoftoys@gmail.com>
2019-01-24 14:39:30 +0400

    Новая версия 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="swtoolz",
-    version="0.0.1",
+    version="0.2.0",
     author="Kirill Belyaev",
     author_email="bandoftoys@gmail.com",
     description="SWToolz-Core API wrapper.",
